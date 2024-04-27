1884abad0bd2b054700b664bae87c5ae6ee93b90
Senanur Emir <102187756+senanuremr@users.noreply.github.com>
2024-04-27 09:07:07 +0300

    Initial Commit

---
+++ My List/My_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_ListApp.swift
+//  My List
+//
+//  Created by Senanur EMİR on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct My_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
